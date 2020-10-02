7332902d2fcba6617a11ef341e8f0781c594cced
kundansingh1988 <72249806+kundansingh1988@users.noreply.github.com>
2020-10-02 13:48:59 +0530

    Add BIT

---
+++ BIT.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct FenwickTree {
+    vector<int> bit;  // binary indexed tree
+    int n;
+
+    FenwickTree(int n) {
+        this->n = n;
+        bit.assign(n, 0);
+    }
+
+    FenwickTree(vector<int> a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    int sum(int r) {
+        int ret = 0;
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bit[r];
+        return ret;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, int delta) {
+        for (; idx < n; idx = (idx | (idx + 1)))
+            bit[idx] += delta;
+    }
+};
+
+int main()
+{
+    //Enter your code here
+    int n,i,j;
+    vector<int> a({1,3,4,6,2});
+    // 0 based indexing
+    FenwickTree bit(a[i]);
+    cout<<bit.sum(0,4)<<"\n";
+    bit.add(2,4);
+    
+    return 0;
+}
